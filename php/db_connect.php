bad1564154a88bfaafb403253efb6d7167d90eb9
DrippKing <christiansalazar2005@gmail.com>
2025-11-26 10:30:59 -0600

    Update db_connect.php

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,9 +1,16 @@
 <?php
-$servername = "Drippking.com";
+$servername = "localhost";
 $username = "drippkin_Host";
 $password = "DrippKing5545";
 $dbname = "drippkin_mt6_world_cup";
 $port = 3306;
+
+/* $servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "drippkin_mt6_world_cup";
+$port = 3307; */
+
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
   die("Conexión fallida: " . $conn->connect_error);
